8b2d53200be09a2bcd4fa91dcba39b66a11cf6bb
tpnx <seb@meerw.de>
2017-02-17 11:00:33 +0100

    Classy random_deck.py

--- random_deck.py
+++ random_deck.py
@@ -1,96 +1,122 @@
 #!/usr/bin/env python2
-
-# import os
-# import requests
-# import sys
-
 from bs4 import BeautifulSoup
 from collections import Counter
 import argparse
 import json
 import random
 
-parser = argparse.ArgumentParser()
-parser.add_argument("hclass")
-parser.add_argument("-cc", type=float)
-parser.add_argument("-w", "--wild", action="store_true")
-args = parser.parse_args()
 
-# variables
-cards_blacklist = []
-cards_collection = []
-class_player = args.hclass
-cards_player = []
+class Card(object):
 
+    def __init__(self, cname, cclass, crarity):
+        self.cname = cname
+        self.cclass = cclass
+        self.crarity = crarity
 
-def get_blacklist():
-    with open("cards.collectible.json") as c:
-        json_request = c.read()
-    json_blacklist = json.loads(json_request)
-    for item in json_blacklist:
-        if (item["set"] == "NAXX") or (item["set"] == "TGT"):
-            cards_blacklist.append(item["name"])
-    c.close()
+    def __str__(self):
+        return '%s, %s, %s' % (self.cname, self.cclass, self.crarity)
 
 
-def get_collection():
+class Deck(object):
+
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        l = sorted(Counter([item.cname for item in self.cards]).items())
+        return '\n'.join(['%d %s' % (count, card) for card, count in l])
+
+    def random(self, cclass=None):
+        if cclass:
+            cards = [c for c in self.cards if c.cclass == cclass]
+        else:
+            cards = self.cards
+        if cards:
+            return random.choice(cards)
+
+    def drop_card(self, card):
+        self.cards.remove(card)
+
+    def take_card(self, card):
+        self.cards.append(card)
+
+    def count_card(self, card):
+        return sum(1 for c in self.cards if c.cname == card)
+
+
+def create_d_collection(cclass, bl=[]):
+    d = Deck()
     with open("collection.html") as f:
-        collection_raw = f.read()
-    f.close()
-    return collection_raw
-
-
-def create_collection(raw_data):
-    collection_data = BeautifulSoup(raw_data, 'html.parser')
-    collection_items = collection_data.findAll("div", {"class": "owns-card"})
-    for item in collection_items:
-        if ((item['data-card-class'] == "NONE") or (item['data-card-class'] == class_player)) and (item['data-card-name'] not in cards_blacklist):
-            card_collection_name = item['data-card-name']
-            card_collection_count = int(item.findAll("span")[-1]['data-card-count'])
-            card_collection_class = item['data-card-class']
-            card_collection_rarity = int(item['data-rarity'])
-            cards_collection.append([card_collection_name, card_collection_class, card_collection_count, card_collection_rarity])
-
-
-def create_random_deck():
-    number_cards = float(30)
-    if args.cc:
-        number_class_cards = int(number_cards/100*args.cc)
-        number_none_cards = int(number_cards - number_class_cards)
-    else:
-        number_class_cards = int(number_cards/100*50)
-        number_none_cards = int(number_cards - number_class_cards)
-    sum_class_cards = 0
-    sum_none_cards = 0
-    i = 0
-    while (i < number_cards):
-        rng = random.randrange(0, len(cards_collection))
-        random_card = cards_collection[rng]
-        if (cards_player.count(random_card[0]) == 2):  # eliminate sum up of golden und non-golden cards
-            cards_collection.pop(rng)
+        c_raw = f.read()
+    c_items = BeautifulSoup(c_raw,
+                            'html.parser').findAll("div",
+                                                   {"class": "owns-card"})
+    for item in c_items:
+        ccount = int(item.findAll("span")[-1]['data-card-count'])
+        c = Card(item['data-card-name'],
+                 item['data-card-class'],
+                 int(item['data-rarity']))
+        if (d.count_card(c.cname) == 2) or ((d.count_card(c.cname) == 1 and c.crarity == 5)) or (c.cname in bl):
             pass
-        elif (random_card[1] == class_player) and (sum_class_cards < number_class_cards):
-            cards_player.append(random_card[0])
+        elif (c.cclass == "NONE") or (c.cclass == cclass):
+            d.take_card(c)
+            if ccount == 2:
+                d.take_card(c)
+    return d
+
+
+def create_l_blacklist():
+    l = []
+    w = ("NAXX", "TGT")
+    with open("cards.collectible.json") as f:
+        j_request = f.read()
+    j_bl = json.loads(j_request)
+    l = [c["name"] for c in j_bl if c["set"] in w]
+    return l
+
+
+def create_d_random(collection, cclass):
+    d = Deck()
+
+    # number_cards = float(30)
+    # if args.cc:
+    #     number_class_cards = int(number_cards/100*args.cc)
+    #     number_none_cards = int(number_cards - number_class_cards)
+    # else:
+    #     number_class_cards = int(number_cards/100*50)
+    #     number_none_cards = int(number_cards - number_class_cards)
+    # sum_class_cards = 0
+    # sum_none_cards = 0
+
+    sum_class_cards = 0
+    i = 0
+    while (i < 30):
+        c = collection.random()
+        if (c.cclass == cclass) and (sum_class_cards < 15):
+            d.take_card(c)
+            collection.drop_card(c)
             sum_class_cards += 1
             i += 1
-        elif (random_card[1] == "NONE") and (sum_none_cards < number_none_cards):
-            cards_player.append(random_card[0])
-            sum_none_cards += 1
+        elif (c.cclass == "NONE") and (sum_class_cards < 15):
+            d.take_card(c)
+            collection.drop_card(c)
             i += 1
-        if (cards_player.count(random_card[0]) == cards_collection[rng][2]) or (random_card[3] == 5):  # maximum number of this card
-            cards_collection.pop(rng)
-            pass
-    deck_player = Counter(cards_player)
-    for card, count in deck_player.iteritems():
-        print str(count) + " " + card
+    return d
 
 
 def main():
-    if args.wild is False:
-        get_blacklist()
-    create_collection(get_collection())
-    create_random_deck()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cclass", type=str)
+    parser.add_argument("-")
+    args = parser.parse_args()
 
+    cclass = str(args.cclass)
+
+    l_blacklist = create_l_blacklist()
+    d_collection = create_d_collection(cclass, l_blacklist)
+    d_random = create_d_random(d_collection, cclass)
+
+    print d_random
 
 if __name__ == '__main__':
     main()
