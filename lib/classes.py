cf6d2f78ebb6439b047892ae4aa23c24f04b048d
tpnx <seb@meerw.de>
2017-02-19 10:12:34 +0100

    moved classes to module

---
+++ lib/classes.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+import random
+
+
+class Card(object):
+
+    def __init__(self, cname, cclass, crarity):
+        self.cname = cname
+        self.cclass = cclass
+        self.crarity = crarity
+
+    def __str__(self):
+        return '%s, %s, %s' % (self.cname, self.cclass, self.crarity)
+
+
+class Deck(object):
+
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        l = sorted(Counter([item.cname for item in self.cards]).items())
+        return '\n'.join(['%d %s' % (count, card) for card, count in l])
+
+    def random(self, cclass=None):
+        if cclass:
+            cards = [c for c in self.cards if c.cclass == cclass]
+        else:
+            cards = self.cards
+        if cards:
+            return random.choice(cards)
+
+    def drop_card(self, card):
+        self.cards.remove(card)
+
+    def take_card(self, card):
+        self.cards.append(card)
+
+    def count_card(self, card):
+        return sum(1 for c in self.cards if c.cname == card)
--- random_deck.py
+++ random_deck.py
@@ -1,47 +1,10 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
 from bs4 import BeautifulSoup
-from collections import Counter
+from lib.classes import Card, Deck
 import argparse
 import json
-import random
-
-
-class Card(object):
-
-    def __init__(self, cname, cclass, crarity):
-        self.cname = cname
-        self.cclass = cclass
-        self.crarity = crarity
-
-    def __str__(self):
-        return '%s, %s, %s' % (self.cname, self.cclass, self.crarity)
-
-
-class Deck(object):
-
-    def __init__(self):
-        self.cards = []
-
-    def __str__(self):
-        l = sorted(Counter([item.cname for item in self.cards]).items())
-        return '\n'.join(['%d %s' % (count, card) for card, count in l])
-
-    def random(self, cclass=None):
-        if cclass:
-            cards = [c for c in self.cards if c.cclass == cclass]
-        else:
-            cards = self.cards
-        if cards:
-            return random.choice(cards)
-
-    def drop_card(self, card):
-        self.cards.remove(card)
-
-    def take_card(self, card):
-        self.cards.append(card)
-
-    def count_card(self, card):
-        return sum(1 for c in self.cards if c.cname == card)
 
 
 def create_d_collection(cclass, bl=[]):
