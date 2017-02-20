9110e0c724af42790f8cae6b9fb05957b61da6f3
tpnx <seb@meerw.de>
2017-02-20 11:03:04 +0100

    deleted obsolete _functions.py

--- _functions.py
+++
@@ -1,8 +0,0 @@
-def write_deck_file(deck_path, deck_list):
-
-    print "\n# Exporting deck to " + deck_path + "\n"
-    out = open(deck_path, 'w')
-    for item in deck_list:
-        out.write(item + "\n")
-        print item
-    out.close()
